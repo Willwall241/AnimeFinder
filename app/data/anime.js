af678cc90b8623ebc1ec67d7034fa887f0c724d4
WJW <will.j.waller@gmail.com>
2018-08-11 17:06:59 -0400

    update logic and layout

---
+++ app/data/anime.js
@@ -0,0 +1,169 @@
+// File that holds Anime Data
+
+var anime = [
+  {
+    animeName:"Sword Art Online",
+    animeImage:"https://myanimelist.cdn-dena.com/images/anime/11/39717.jpg",
+    scores: [
+      5,
+      5,
+      5,
+      3,
+      5,
+      1,
+      3,
+      5,
+      2,
+      5
+    ]
+  },
+  {
+    animeName:"Tokyo Ghoul",
+    animeImage:"https://myanimelist.cdn-dena.com/images/anime/5/64449.jpg",
+    scores: [
+      5,
+      5,
+      3,
+      1,
+      2,
+      5,
+      5,
+      1,
+      5,
+      1
+    ]
+  },
+  {
+    animeName:"Soul Eater",
+    animeImage:"https://myanimelist.cdn-dena.com/r/167x242/images/anime/9/7804.webp?s=e0ad63ea8540aa35e6f869b3e6207b2f",
+    scores: [
+      5,
+      3,
+      2,
+      4,
+      5,
+      5,
+      3,
+      1,
+      2,
+      2
+    ]
+  },
+  {
+    animeName:"InuYasha",
+    animeImage:"https://myanimelist.cdn-dena.com/r/167x242/images/anime/8/20483.webp?s=8ff57bc602541bbbfca3244c6629a852",
+    scores: [
+      5,
+      1,
+      4,
+      3,
+      5,
+      4,
+      4,
+      2,
+      2,
+      2
+    ]
+  },
+  {
+    animeName:"Sailor Moon",
+    animeImage:"https://myanimelist.cdn-dena.com/images/anime/1440/92258.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      4,
+      5,
+      4,
+      3,
+      5
+    ]
+  },
+  {
+    animeName:"Kids on the Slope",
+    animeImage:"http://www.japansubculture.com/wp-content/uploads/2015/06/Screen-Shot-2015-06-16-at-1.06.58-AM-440x249.png",
+    scores: [
+      1,
+      5,
+      5,
+      4,
+      5,
+      1,
+      3,
+      1,
+      3,
+      5
+    ]
+  },
+  {
+    animeName:"Higashi no Eden",
+    animeImage:"https://myanimelist.cdn-dena.com/r/167x242/images/anime/9/15033.webp?s=6184f05d76235c24826c543e1ff9afc8",
+    scores: [
+      1,
+      2,
+      5,
+      4,
+      3,
+      1,
+      1,
+      1,
+      2,
+      5
+    ]
+  },
+  {
+    animeName:"One Punch Man",
+    animeImage:"https://myanimelist.cdn-dena.com/images/anime/12/76049.jpg",
+    scores: [
+      5,
+      5,
+      1,
+      5,
+      4,
+      5,
+      1,
+      1,
+      3,
+      1
+    ]
+  },
+  {
+    animeName:"Cardcaptor Sakura",
+    animeImage:"https://myanimelist.cdn-dena.com/r/167x242/images/anime/8/60781.webp?s=1ab8d0e7bf7b9168014bd9a73be5fb02",
+    scores: [
+      5,
+      1,
+      4,
+      3,
+      5,
+      3,
+      5,
+      5,
+      1,
+      4
+    ]
+  },
+  {
+    animeName:"Tenkuu no Escaflowne",
+    animeImage:"https://myanimelist.cdn-dena.com/r/167x242/images/anime/5/30689.webp?s=393897d9e8b39daa6142366f50adbec4",
+    scores: [
+      5,
+      1,
+      4,
+      2,
+      5,
+      4,
+      3,
+      3,
+      2,
+      3
+    ]
+  }
+  
+
+]
+
+// Export for array
+module.exports = anime;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+// Load data and API routes
+
+// Require js file that contains anime data
+var anime = require("../data/anime.js");
+
+// Routes
+
+module.exports = function(app) {
+  //a GET route that displays JSON of all possible friends
+  app.get('/api/anime', function(req,res){
+    res.json(anime);
+  });
+
+  app.post('/api/anime', function(req,res){
+    var userInput = req.body.scores;
+    var scoreDiffArray = [];
+    var match = 0;
+
+    for(var i=0; i < anime.length; i++) {
+
+      var scoreDifference = 0;
+
+      for (var j=0; j < userInput.length; j++) {
+        scoreDifference += (Math.abs(parseInt(anime[i].scores[j]) - parseInt(userInput[j])));
+      }
+
+      scoreDiffArray.push(scoreDifference);
+
+    }
+
+    for(var i=0; i < scoreDiffArray.length; i++){
+      if(scoreDiffArray[i] <= scoreDiffArray[match]){
+        match = i;
+      }
+    }
+
+    var best = anime[match]
+    res.json(best);
+ 
+  });
+
+}
