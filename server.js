dfbbadc7a2521fd2727bada251ebe59090be7075
WJW <will.j.waller@gmail.com>
2018-08-11 18:30:20 -0400

    update to allow link to css file

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/app/public"));
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 //ROUTER
 require('./app/routing/apiRoutes.js')(app); 
